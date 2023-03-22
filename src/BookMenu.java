1f0a56332d641932e5d1a0c011082ff93344b010
kimijammy <125917969+kimijammy@users.noreply.github.com>
2023-03-22 12:57:25 +0000

    Update BookMenu.java

--- src/BookMenu.java
+++ src/BookMenu.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class BookMenu {
+public class BookMenu {
+    public static void main(String[] args) {
+        BookStore customer1 = new BookStore("Spider-Man","Stan Lee", 15.99,1);
+        BookStore customer2= new BookStore("Wolverine Origins", "DC Comics", 14.99,1);
+
+        System.out.println("I went to the store and bought "+customer1.getTitle() +" and " +customer2.getTitle());
+        System.out.println(customer1.getTotal());
+    }
 }
